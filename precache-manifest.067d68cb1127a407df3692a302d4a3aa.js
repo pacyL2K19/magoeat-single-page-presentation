38ccef7be24f35e5834c2d215b2088d710aeba9c
Pacifique Linjanja <pacilinja1@icloud.com>
2020-09-07 11:18:33 +0200

    Updates

---
+++ precache-manifest.067d68cb1127a407df3692a302d4a3aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2c68901cf3f705fd815eca01102dfe",
+    "url": "/magoeat-single-page-presentation/index.html"
+  },
+  {
+    "revision": "7812c3f544cb8d771765",
+    "url": "/magoeat-single-page-presentation/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "962dbed5418a070708ba",
+    "url": "/magoeat-single-page-presentation/static/js/2.48992afd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magoeat-single-page-presentation/static/js/2.48992afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7812c3f544cb8d771765",
+    "url": "/magoeat-single-page-presentation/static/js/main.5275fe88.chunk.js"
+  },
+  {
+    "revision": "a59bea5b83282e69f410",
+    "url": "/magoeat-single-page-presentation/static/js/runtime-main.dfbb2d0f.js"
+  }
+]);
