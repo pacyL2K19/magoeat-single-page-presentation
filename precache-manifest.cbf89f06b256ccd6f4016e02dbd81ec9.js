f418bd631489bd8986b8ac51d6d554748830ef51
Pacifique Linjanja <pacilinja1@icloud.com>
2020-09-08 17:04:14 +0200

    Updates

---
+++ precache-manifest.cbf89f06b256ccd6f4016e02dbd81ec9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8509040e302a04f8d9bee16980e9b02",
+    "url": "/magoeat-single-page-presentation/index.html"
+  },
+  {
+    "revision": "5df750e8b566d58cd91c",
+    "url": "/magoeat-single-page-presentation/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c0768943ed72b9efb71d",
+    "url": "/magoeat-single-page-presentation/static/js/2.9ae5142f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magoeat-single-page-presentation/static/js/2.9ae5142f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df750e8b566d58cd91c",
+    "url": "/magoeat-single-page-presentation/static/js/main.ccd62b25.chunk.js"
+  },
+  {
+    "revision": "a59bea5b83282e69f410",
+    "url": "/magoeat-single-page-presentation/static/js/runtime-main.dfbb2d0f.js"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/magoeat-single-page-presentation/static/media/android.db9b21a1.png"
+  }
+]);
